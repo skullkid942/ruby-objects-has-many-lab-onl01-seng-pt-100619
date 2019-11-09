73e7cefc204755f11a53e6c0d993671eabcfdac6
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-09 18:30:32 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,31 @@
+class Author 
+  @@post_count = 0 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(a_post)
+    @posts << a_post
+    a_post.author = self
+    @@post_count += 1 
+  end 
+  
+  def add_post_by_title(title)
+    a_post = Post.new(title)
+    @posts << a_post
+    a_post.author = self
+    @@post_count += 1 
+  end
+  
+  def self.posts
+    @posts
+  end 
+  
+  def self.post_count
+    @@post_count
+  end 
+
+end
